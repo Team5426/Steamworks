e5e440e5ab7a2f9c51562a6762c92689aa9f634e
Duncan S <Sniddunc@users.noreply.github.com>
2017-02-18 01:17:54 -0500

    Update Robot.java

--- src/org/usfirst/frc/team5426/robot/Robot.java
+++ src/org/usfirst/frc/team5426/robot/Robot.java
@@ -13,10 +13,38 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  */
 public class Robot extends IterativeRobot {
     
+    private static final int IMG_WIDTH = 320;
+	private static final int IMG_HEIGHT = 240;
+	
+	private VisionThread vt;
+	private double centerX = 0.0;
+	private RobotDrive drive;
+	
+	private final Object imagelock = new Object();
+    
     public void robotInit() {
 
     	RobotMap.init();
         CommandBase.init();
+        
+        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+        camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
+    
+        vt = new VisionThread(camera, new Pipeline(), pipeline -> {
+            
+            if (!pipeline.filterContoursOutput().isEmpty()) {
+                
+                Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+                
+                synchronized (imagelock) {
+                    centerX = r.x + (r.width / 2);
+                }
+            }
+        });
+        
+        visionThread.start();
+        
+        drive = new RobotDrive(1, 2);
     }
 
     public void disabledInit() {
@@ -47,9 +75,21 @@ public class Robot extends IterativeRobot {
     public void autonomousPeriodic() {
     	
     	Scheduler.getInstance().run();
+        
+        double centerX;
+        
+	    synchronized (imagelock) {
+            
+		    centerX = this.centerX;
+	    }
+        
+	    double turn = centerX - (IMG_WIDTH / 2);
+        
+	    drive.arcadeDrive(-0.6, turn * 0.005);
     }
     
     public void testPeriodic() {
+        
         LiveWindow.run();
     }
 }
